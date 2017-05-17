607ec2d1557fa1de808d791f9d3cc33880f2f687
Anton Mukovozov <graalogosh@gmail.com>
2017-05-17 23:40:04 +0300

    Predefined arrays fix

--- RangeSensor.h
+++ RangeSensor.h
@@ -6,9 +6,9 @@
 class RangeSensor{
 private:
 	#define LENGHT 11
-		static const float voltageArray[LENGHT] = { 2.75f, 2.3f, 1.65f, 1.3f, 1.1f, 0.92f, 0.75f, 0.6f, 0.5f, 0.45f, 0.4f };
-		static const float distanceArray[LENGHT] = { 8.0f, 10.0f, 15.0f, 20.0f, 25.0f, 30.0f, 40.0f, 50.0f, 60.0f, 70.0f, 80.0f };
-		static const unsigned int arrayLength = LENGHT;
+		const float voltageArray[LENGHT] = { 2.75f, 2.3f, 1.65f, 1.3f, 1.1f, 0.92f, 0.75f, 0.6f, 0.5f, 0.45f, 0.4f };
+		const float distanceArray[LENGHT] = { 8.0f, 10.0f, 15.0f, 20.0f, 25.0f, 30.0f, 40.0f, 50.0f, 60.0f, 70.0f, 80.0f };
+		const unsigned int arrayLength = LENGHT;
 	#undef LENGHT
 
 	unsigned int rangePin;
