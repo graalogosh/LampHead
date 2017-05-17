1b277390cd9d23513ce2e51f4f5e46939be7fed9
Anton Mukovozov <graalogosh@gmail.com>
2017-05-17 15:11:51 +0300

    Static values

--- Firmware.ino
+++ Firmware.ino
@@ -1,3 +1,16 @@
+//LED pins
 static const unsigned int redPin = 10;
 static const unsigned int greenPin = 9;
-static const unsigned int bluePin = 11
+static const unsigned int bluePin = 11;
+
+//range sensor signal
+static const unsigned int rangePin = A7;
+
+//minimal and maximum height in sm
+static const unsigned int minHeight = 8;//TODO - change to real value
+static const unsigned int maxHeight = 40;
+
+//light increase and reduce speed
+static const unsigned int reduceSpeed = 5;
+static const unsigned int increaceSpeed = 5;
+
